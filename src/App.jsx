a705ef01ab31a5b697019ca6f5458b989628ad49
dragoon4890 <harshsks123@gmail.com>
2025-12-02 05:49:52 +0530

    Fix: Conditional base path for dev/prod and Router basename

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Blog from './pages/Blog';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
